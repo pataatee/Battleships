1382fa093098875041e090af49c7d68bc4f44be0
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-24 17:18:30 +0100

    Add observer Methode

--- BatailleNavale/src/models/placeable/boat/BoatsObserver.java
+++ BatailleNavale/src/models/placeable/boat/BoatsObserver.java
@@ -1,4 +1,8 @@
 package models.placeable.boat;
 
+import models.state.State;
+
 public interface BoatsObserver {
+    public void notifyOnDeath(Boolean life);
+    public void updateTileState(State state);
 }
