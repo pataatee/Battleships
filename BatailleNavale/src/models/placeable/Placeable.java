b009c762e4e3b91e5e501c8642f0ad5ca9f3f6b4
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-24 14:59:39 +0100

    placeable: completed enum and created constructors

--- BatailleNavale/src/models/placeable/Placeable.java
+++ BatailleNavale/src/models/placeable/Placeable.java
@@ -3,4 +3,9 @@ package models.placeable;
 public abstract class Placeable {
     private int _size;
     private String _name;
+
+    public Placeable(String name, int size) {
+        this._name=name;
+        this._size=size;
+    }
 }
--- BatailleNavale/src/models/placeable/boat/BoatType.java
+++ BatailleNavale/src/models/placeable/boat/BoatType.java
@@ -1,4 +1,19 @@
 package models.placeable.boat;
 
 public enum BoatType {
+    AIRCRAFTCARRIER(5),
+    CRUISER(4),
+    DESTROYER(3),
+    SUBMARINE(3),
+    TORPEDOBOAT(2);
+
+    private int _size;
+
+    BoatType(int size) {
+        this._size=size;
+    }
+
+    public int getSize() {
+        return this._size;
+    }
 }
