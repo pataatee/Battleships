463948d13cc690d6e000546a1c7ca7eba46dc38f
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-24 15:19:49 +0100

    placeable: added pvs to Boat class as an attribute

--- BatailleNavale/src/models/placeable/boat/Boat.java
+++ BatailleNavale/src/models/placeable/boat/Boat.java
@@ -4,8 +4,11 @@ import models.placeable.Placeable;
 
 public abstract class Boat extends Placeable {
     private Boolean _isAlive;
+    private int _pvs;
 
     public Boat(String name,int size) {
         super(name,size);
+        this._isAlive=true;
+        this._pvs=size;
     }
 }
