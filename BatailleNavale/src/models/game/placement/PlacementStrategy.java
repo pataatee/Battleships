06fe76f66049d4c9de4099001bad642e5934b0f3
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-30 00:11:17 +0100

    game: created placement methods (not coded yet), deleted Placement's 'default version' constructor

--- BatailleNavale/src/models/game/placement/PlacementStrategy.java
+++ BatailleNavale/src/models/game/placement/PlacementStrategy.java
@@ -2,9 +2,20 @@ package models.game.placement;
 
 import models.grid.Grid;
 import models.placeable.PlaceableFactory;
+import models.placeable.boat.Boat;
+import models.placeable.trap.Trap;
 import models.player.Player;
 
 public abstract class PlacementStrategy extends PlaceableFactory {
     private Grid _grid;
     private Player _player;
+
+    public PlacementStrategy(Grid grid, Player player) {
+        this._grid = grid;
+        this._player = player;
+    }
+
+    public abstract void placeBoats(Boat[] boats);
+
+    public abstract void placeTraps(Trap[] traps);
 }
