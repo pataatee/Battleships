daf841e152ed8ec2648fc4ec465d44f0c3c5a141
MiaouZart <gauthier.claudel@etu.unistra.fr>
2025-11-29 16:02:32 +0100

    Base Player Class added

--- BatailleNavale/src/models/player/PlayerObserver.java
+++ BatailleNavale/src/models/player/PlayerObserver.java
@@ -1,10 +1,10 @@
 package models.player;
 
-//import models.weapon.Weapon;
+import models.weapon.Weapon;
 
 public interface PlayerObserver {
-//    public void notifyShoot(int x, int y, Weapon weapon);
-//    public void notifyHit(ShotResult[] result);
+    public void notifyShoot(int x, int y, Weapon weapon);
+    public void notifyHit(ShotResult[] result);
     public void notifyEndTurn();
     public void notifyDeath(boolean death);
 
