608846d785077e01f81358f0ee05d66225c207e4
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-29 21:46:51 +0100

    placeable: implemented method notifyObserver

--- BatailleNavale/src/models/placeable/boat/Boat.java
+++ BatailleNavale/src/models/placeable/boat/Boat.java
@@ -2,13 +2,22 @@ package models.placeable.boat;
 
 import models.placeable.Placeable;
 
+import java.util.ArrayList;
+
 public abstract class Boat extends Placeable {
     private Boolean _isAlive;
     private int _pvs;
+    private ArrayList<BoatsObserver> boatObservers;
 
     public Boat(String name,int size) {
         super(name,size);
         this._isAlive=true;
         this._pvs=size;
+    }
+
+    public void notifyObserver(Boolean life) {
+        for (BoatsObserver obs : boatObservers) {
+            obs.notifyOnDeath(life);
+        }
     }
 }
