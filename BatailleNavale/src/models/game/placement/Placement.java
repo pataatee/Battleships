7abcc900d7b76f17cdaa4bf6d03a493941d8d693
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-11-29 23:40:10 +0100

    game: implemented Placement's constructors

--- BatailleNavale/src/models/game/placement/Placement.java
+++ BatailleNavale/src/models/game/placement/Placement.java
@@ -1,4 +1,32 @@
 package models.game.placement;
 
+import models.grid.Grid;
+import models.player.Player;
+
 public class Placement {
+
+    private Grid[] _grids;
+    private Player[] _players;
+    private PlacementStrategy _placementStrategy;
+
+    //choose everything version
+    public Placement(Grid[] grids, Player[] players, PlacementStrategy strat) {
+        this._grids = grids;
+        this._players = players;
+        this._placementStrategy = strat;
+    }
+
+    // only choose strategy version
+    public Placement(PlacementStrategy strat) {
+        this._grids = new Grid[2];
+        this._players = new Player[2];
+        this._placementStrategy = strat;
+    }
+
+    // default values version
+    public Placement() {
+        this._grids = new Grid[2];
+        this._players = new Player[2];
+        this._placementStrategy = new ManualPlacementStrategy();
+    }
 }
--- BatailleNavale/src/models/game/placement/PlacementStrategy.java
+++ BatailleNavale/src/models/game/placement/PlacementStrategy.java
@@ -1,9 +1,10 @@
 package models.game.placement;
 
 import models.grid.Grid;
+import models.placeable.PlaceableFactory;
 import models.player.Player;
 
-public abstract class PlacementStrategy {
+public abstract class PlacementStrategy extends PlaceableFactory {
     private Grid _grid;
     private Player _player;
 }
