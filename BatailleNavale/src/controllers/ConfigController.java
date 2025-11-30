e2d20f48aaccd324bae7f7330d0b304c41b74503
MiaouZart <gauthier.claudel@etu.unistra.fr>
2025-11-30 14:55:43 +0100

    ConfigScreen added with ConfigData base code and ConfigController , might need to add ConfigData Observer

---
+++ BatailleNavale/src/controllers/ConfigController.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import models.game.GameMode;
+import models.game.placement.ConfigData;
+import models.placeable.boat.Boat;
+import models.placeable.boat.BoatType;
+
+public class ConfigController {
+    private final ConfigData _mdData;
+
+    public ConfigController(ConfigData mdData) {
+        _mdData = mdData;
+    }
+
+    /**
+     * Try to select a boat for the game. If the boat cannot be added, return false
+     * @param boat The boat to add
+     * @return true if the boat was added, false otherwise
+     */
+    public boolean selectBoat(Boat boat) {
+        return _mdData.addBoat(boat);
+    }
+
+    /**
+     * Deselect (remove) the last boat of the specified type
+     * @param boatType The type of boat to remove
+     * @return true if a boat was removed, false otherwise
+     */
+    public boolean deselectBoat(BoatType boatType) {
+        return _mdData.removeBoat(boatType);
+    }
+
+    /**
+     * Get the total number of cells occupied by all selected boats
+     * @return The total number of cells
+     */
+    public int getTotalCases() {
+        return _mdData.getTotalCells();
+    }
+
+    /**
+     * Get the number of boats of a specific type
+     * @param boatType The type of boat
+     * @return The count of boats of this type
+     */
+    public int getBoatCount(BoatType boatType) {
+        return _mdData.getBoatCountByType(boatType);
+    }
+
+    /**
+     * Set the game mode
+     * @param gameMode The game mode to set
+     */
+    public void SelectGameMode(GameMode gameMode) {
+        _mdData.setGameMode(gameMode);
+    }
+
+    /**
+     * Set the grid size
+     * @param size The size of the grid
+     */
+    public void setGridSize(int size) {
+        _mdData.setGridSize(size);
+    }
+}
--- BatailleNavale/src/models/game/placement/ConfigData.java
+++ BatailleNavale/src/models/game/placement/ConfigData.java
@@ -1,4 +1,84 @@
 package models.game.placement;
 
+import models.game.GameMode;
+import models.grid.Grid;
+import models.placeable.boat.Boat;
+import models.placeable.boat.BoatType;
+
+import java.util.ArrayList;
+
 public class ConfigData {
+    private static final int MAXCELL = 35;
+
+    private int _gridSize;
+    private ArrayList<Boat> _boatList;
+    private Grid[] _grids;
+    private int _sumCell;
+    private GameMode _gm;
+
+    public ConfigData(Grid[] grid) {
+        _grids = grid;
+        _sumCell = 0;
+        _gm = GameMode.NORMAL;
+        _boatList = new ArrayList<Boat>();
+    }
+
+    public void setGridSize(int gridSize) {
+        for (Grid grid : _grids) {
+            grid.setSize(gridSize);
+        }
+        _gridSize = gridSize;
+    }
+
+    public int getGridSize() {
+        return _gridSize;
+    }
+
+    public boolean addBoat(Boat boat) {
+        _sumCell += boat.getSize();
+        if (_sumCell > MAXCELL) {
+            _sumCell -= boat.getSize();
+            return false;
+        }
+        _boatList.add(boat);
+        return true;
+    }
+
+    public boolean removeBoat(BoatType boatType) {
+        for (int i = _boatList.size() - 1; i >= 0; i--) {
+            Boat boat = _boatList.get(i);
+            if (boat.getBoatType() == boatType) {
+                _boatList.remove(i);
+                _sumCell -= boat.getSize();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getTotalCells() {
+        return _sumCell;
+    }
+
+    public int getBoatCountByType(BoatType boatType) {
+        int count = 0;
+        for (Boat boat : _boatList) {
+            if (boat.getBoatType() == boatType) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public ArrayList<Boat> getChosenBoats() {
+        return _boatList;
+    }
+
+    public void setGameMode(GameMode gm) {
+        _gm = gm;
+    }
+
+    public GameMode getGameMode() {
+        return _gm;
+    }
 }
