9dfaa96265ff348f7a35f0119e89ebeb9ba3bd3e
MiaouZart <gauthier.claudel@etu.unistra.fr>
2025-11-30 14:55:43 +0100

    ConfigScreen added with ConfigData base code and ConfigController , might need to add ConfigData Observer

--- BatailleNavale/src/Main.java
+++ BatailleNavale/src/Main.java
@@ -1,5 +1,24 @@
+import controllers.ConfigController;
+import models.game.placement.ConfigData;
+import models.grid.Grid;
+import views.ConfigScreen;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Grid[] grids = new Grid[2];
+        grids[0] = new Grid(5);
+        grids[1] = new Grid(5);
+
+        ConfigData conf = new ConfigData(grids);
+
+        ConfigController configController = new ConfigController(conf);
+
+
+        ConfigScreen configScreen = new ConfigScreen(configController);
+        configScreen.setVisible(true);
+
+
     }
 }
