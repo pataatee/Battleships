8638b25d8e616d214e7de311630c8a8b3f59b72c
Manon Tranchant-Berthomieux <manon.tranchant-berthomieux@etu.unistra.fr>
2025-12-18 21:01:26 +0100

    Refactored code + added the Random option to the placementView

---
+++ BatailleNavale/src/views/RandomPlacementPanel.java
@@ -0,0 +1,25 @@
+package views;
+
+import controllers.PlacementController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RandomPlacementPanel extends JPanel {
+
+    private PlacementController _pc;
+    private BordelPanel _pnlGrid;
+
+    public RandomPlacementPanel(PlacementController pc, BordelPanel bp) {
+
+        this._pc = pc;
+        this._pnlGrid = bp;
+
+        this.setLayout(new FlowLayout());
+        this.add(_pnlGrid);
+
+    }
+
+
+
+}
